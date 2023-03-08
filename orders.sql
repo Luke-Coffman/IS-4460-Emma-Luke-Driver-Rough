163462adbbc0f53be8332350ba9fbde0db1fdf52
JaxonDriver <31314439+JaxonDriver@users.noreply.github.com>
2023-03-08 17:16:00 +0100

    Add files via upload

--- orders.sql
+++ orders.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 07, 2023 at 03:49 PM
--- Server version: 8.0.31
--- PHP Version: 8.0.26
+-- Host: localhost:8889
+-- Generation Time: Mar 08, 2023 at 04:14 PM
+-- Server version: 5.7.39
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `kitten factory database`
+-- Database: `Kitten Factory Database`
 --
 
 -- --------------------------------------------------------
@@ -27,44 +27,61 @@ SET time_zone = "+00:00";
 -- Table structure for table `orders`
 --
 
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE IF NOT EXISTS `orders` (
-  `Order_ID` int NOT NULL AUTO_INCREMENT,
-  `EMP_ID` int NOT NULL,
-  `CUST_ID` int NOT NULL,
+CREATE TABLE `orders` (
+  `Order_ID` int(11) NOT NULL,
+  `EMP_ID` int(11) NOT NULL,
+  `CUST_ID` int(11) NOT NULL,
   `DATE` date NOT NULL,
   `Total_Price` decimal(10,2) NOT NULL,
-  `PMT_ID` int NOT NULL,
-  `Description` varchar(256) NOT NULL,
-  PRIMARY KEY (`Order_ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `PMT_ID` int(11) NOT NULL,
+  `Description` varchar(256) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `orders`
 --
 
 INSERT INTO `orders` (`Order_ID`, `EMP_ID`, `CUST_ID`, `DATE`, `Total_Price`, `PMT_ID`, `Description`) VALUES
-(1, 1, 1, '2022-03-05', '535.99', 1, 'Kitten Powder Skis Black 150  '),
-(2, 1, 2, '0000-00-00', '1250.99', 2, 'KittenSkis Park 160 Yellow'),
-(3, 1, 3, '0000-00-00', '1250.99', 3, 'KittenSkis Park 160 Yellow'),
-(4, 1, 3, '0000-00-00', '1250.99', 4, 'KittenSkis Park 160 Yellow'),
-(5, 1, 5, '0000-00-00', '1250.99', 5, 'KittenSkis Park 160 Yellow'),
-(6, 1, 1, '0000-00-00', '1250.99', 6, 'KittenSkis Park 160 Yellow'),
-(7, 1, 5, '0000-00-00', '1250.99', 7, 'KittenSkis Park 160 Yellow'),
-(8, 1, 8, '0000-00-00', '1250.99', 8, 'KittenSkis Park 160 Yellow'),
-(9, 1, 5, '0000-00-00', '1250.99', 9, 'KittenSkis Park 160 Yellow'),
-(10, 1, 4, '0000-00-00', '1250.99', 10, 'KittenSkis Park 160 Yellow'),
-(11, 1, 1, '0000-00-00', '1250.99', 11, 'KittenSkis Park 160 Yellow'),
-(12, 1, 10, '0000-00-00', '1250.99', 12, 'KittenSkis Park 160 Yellow'),
-(13, 1, 3, '0000-00-00', '1250.99', 13, 'KittenSkis Park 160 Yellow'),
-(14, 1, 15, '0000-00-00', '1250.99', 14, 'KittenSkis Park 160 Yellow'),
-(15, 1, 8, '0000-00-00', '1250.99', 15, 'KittenSkis Park 160 Yellow'),
-(16, 1, 7, '0000-00-00', '1250.99', 16, 'KittenSkis Park 160 Yellow'),
-(17, 1, 12, '0000-00-00', '1250.99', 17, 'KittenSkis Park 160 Yellow'),
-(18, 1, 6, '0000-00-00', '1250.99', 18, 'KittenSkis Park 160 Yellow'),
-(19, 1, 1, '0000-00-00', '1250.99', 19, 'KittenSkis Park 160 Yellow'),
-(20, 1, 11, '0000-00-00', '1250.99', 20, 'KittenSkis Park 160 Yellow'),
-(21, 1, 10, '0000-00-00', '1250.99', 21, 'KittenSkis Park 160 Yellow');
+(1, 1, 1, '2023-03-05', '535.99', 1, 'Order contains 1 or more of: Boots, Skis, Hat'),
+(2, 2, 2, '2023-01-17', '3001.23', 2, 'Order contains 1 or more of: Bindings, Skis, Hat'),
+(3, 3, 3, '2023-02-23', '1269.73', 3, 'Order contains 1 or more of: Gloves, Skis, Hat'),
+(4, 4, 4, '2023-03-01', '2354.92', 4, 'Order contains 1 or more of: Boots, Skis, Gloves'),
+(5, 5, 5, '2023-02-07', '1138.79', 5, 'Order contains 1 or more of: Neckwarmer, Skis, Hat'),
+(6, 6, 6, '2023-01-31', '4717.62', 6, 'Order contains 1 or more of: Gloves, Skis, Water Bottle'),
+(7, 7, 7, '2022-12-28', '129.31', 7, 'Order contains 1 or more of: Lift Day Pass, Skis, Hat'),
+(8, 8, 8, '2023-02-15', '3816.29', 8, 'Order contains 1 or more of: Chaptstick, Skis, Hat'),
+(9, 9, 9, '2023-01-27', '972.18', 9, 'Order contains 1 or more of: Gloves, Skis, Sunglasses'),
+(10, 10, 10, '2023-01-23', '2489.83', 10, 'Order contains 1 or more of: Skis, Trail Mix, Hat'),
+(11, 11, 11, '2022-12-25', '2753.28', 11, 'Order contains 1 or more of: Boots, Skis, Hat'),
+(12, 12, 12, '2023-01-19', '575.02', 12, 'Order contains 1 or more of: Gloves, Skis, Hat'),
+(13, 13, 13, '2023-02-13', '4006.83', 13, 'Order contains 1 or more of: Helmet, Skis, Gloves'),
+(14, 14, 14, '2023-01-24', '4471.20', 14, 'Order contains 1 or more of: Backpack, Helmet, Skis'),
+(15, 15, 15, '2023-02-04', '418.85', 15, 'Order contains 1 or more of: Transceiver, Skis, Hat'),
+(16, 16, 16, '2023-01-18', '3069.20', 16, 'Order contains 1 or more of: Boots, Skis, Hat'),
+(17, 17, 17, '2023-02-14', '142.37', 17, 'Order contains 1 or more of: Helmet, Gloves, Skis, '),
+(18, 18, 18, '2023-02-16', '4397.84', 18, 'Order contains 1 or more of: Skis, Lift Day Pass, Hat'),
+(19, 19, 19, '2023-01-08', '1388.93', 19, 'Order contains 1 or more of: Sunglasses, Skis, Hat'),
+(20, 20, 20, '2023-02-22', '3592.00', 20, 'Order contains 1 or more of: Helmet, Skis, Gloves');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`Order_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `Order_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
